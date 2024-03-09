5658320bc9321f881addf29f971a70c0b92c8439
Alan Candido <brodao@gmail.com>
2024-03-09 20:34:27 -0300

    Documentação (cabeçalho de licenciamento)

--- extension/src/debug/configurationProvider.ts
+++ extension/src/debug/configurationProvider.ts
@@ -1,16 +1,23 @@
-import {
-  WorkspaceFolder,
-  DebugConfigurationProvider,
-  DebugConfiguration,
-  CancellationToken,
-  window,
-} from "vscode";
+/*
+Copyright 2024 Alan Cândido (brodao@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http: //www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import * as vscode from "vscode";
 
-export class SuDebugConfigurationProvider implements DebugConfigurationProvider {
+export class SuDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
   static TYPE: string = "aclab-su-rdebug";
-
-  //protected _server?: Net.Server;
 
   constructor() {
 
@@ -21,10 +28,10 @@ export class SuDebugConfigurationProvider implements DebugConfigurationProvider 
    * e.g. add all missing attributes to the debug configuration.
    */
   async resolveDebugConfiguration(
-    folder: WorkspaceFolder | undefined,
-    config: DebugConfiguration,
-    token?: CancellationToken
-  ): Promise<DebugConfiguration> {
+    folder: vscode.WorkspaceFolder | undefined,
+    config: vscode.DebugConfiguration,
+    token?: vscode.CancellationToken
+  ): Promise<vscode.DebugConfiguration> {
 
     if (folder) {
       config.workspaceFolders = folder;
@@ -50,26 +57,26 @@ export class SuDebugConfigurationProvider implements DebugConfigurationProvider 
     return Promise.resolve(config);
   }
 
-  protected initialize(config: DebugConfiguration) {
-    //   config.type = SuDebugConfigurationProvider.TYPE;
-    //   config.name = "._NAME"; 
-  }
+  // protected initialize(config: vscode.DebugConfiguration) {
+  //   //   config.type = SuDebugConfigurationProvider.TYPE;
+  //   //   config.name = "._NAME"; 
+  // }
 
-  protected finalize(config: DebugConfiguration) {
+  // protected finalize(config: vscode.DebugConfiguration) {
 
-    return config;
-  }
+  //   return config;
+  // }
 
   resolveDebugConfigurationWithSubstitutedVariables(
-    folder: WorkspaceFolder | undefined,
-    debugConfiguration: DebugConfiguration,
-    token?: CancellationToken
-  ): vscode.ProviderResult<DebugConfiguration> {
+    folder: vscode.WorkspaceFolder | undefined,
+    debugConfiguration: vscode.DebugConfiguration,
+    token?: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.DebugConfiguration> {
     if (token?.isCancellationRequested) {
       return undefined;
     }
 
-    return this.finalize(debugConfiguration);
+    return debugConfiguration;
   }
 
   dispose() {
--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,75 +1,37 @@
+/*
+Copyright 2024 Alan Cândido (brodao@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http: //www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import * as vscode from 'vscode';
 import { SuDebugConfigurationProvider } from './debug/configurationProvider';
 import { SuDebugAdapterDescriptorFactory, stopSketchUp } from './debug/debugAdapterDescriptorFactory';
 import { SuDebugAdapterTrackerFactory } from './debug/debugTrackerDescriptorFactory';
-import * as path from 'path';
 
-//import { registerInspectorView } from "./inspector";
-
-//const asyncExec = promisify(child_process.exec);
-
+const SU_TYPE_DEBUG: string = "aclab-su-rdebug";
 let outputChannel: vscode.OutputChannel;
 
 export function pp(obj: any, obj1?: any) {
 	let text: string = JSON.stringify(obj);
 
 	if (obj1) {
-		text += "\n" + JSON.stringify(obj1) + "\n";
+		text += "\n" + JSON.stringify(obj1, undefined, 2) + "\n";
 	}
 
 	outputChannel.appendLine(text);
 }
 
-//const outputTerminals = new Map<string, vscode.Terminal>();
-// let lastExecCommand: string | undefined;
-// let lastProgram: string | undefined;
-
-//const terminalName: string = "Sketch Debug Terminal";
-
-// function workspaceFolder(): string | undefined {
-// 	if (vscode.workspace.workspaceFolders) {
-// 		for (const ws of vscode.workspace.workspaceFolders) {
-// 			return ws.uri.fsPath;
-// 		}
-// 	}
-// }
-
-// function customPath(workingDirectory: string): string {
-// 	if (path.isAbsolute(workingDirectory)) {
-// 		return workingDirectory;
-// 	} else {
-// 		const wspath = workspaceFolder();
-
-// 		if (wspath) {
-// 			return path.join(wspath, workingDirectory);
-// 		} else {
-// 			return workingDirectory;
-// 		}
-// 	}
-// }
-
-// function exportBreakpoints() {
-// 	if (vscode.workspace.getConfiguration("rdbg").get("saveBreakpoints")) {
-// 		const wspath = workspaceFolder();
-
-// 		if (wspath) {
-// 			var bpLines = "";
-// 			for (const bp of vscode.debug.breakpoints) {
-// 				if (bp instanceof vscode.SourceBreakpoint && bp.enabled) {
-// 					// outputChannel.appendLine(JSON.stringify(bp));
-// 					const startLine = bp.location.range.start.line + 1;
-// 					const path = bp.location.uri.path;
-// 					bpLines = bpLines + "break " + path + ":" + startLine + "\n";
-// 				}
-// 			}
-// 			const bpPath = path.join(wspath, ".rdbgrc.breakpoints");
-// 			fs.writeFile(bpPath, bpLines, () => { });
-// 			outputChannel.appendLine("Written: " + bpPath);
-// 		}
-// 	}
-// }
-
-const SU_TYPE_DEBUG: string = "aclab-su-rdebug";
 
 export function activate(context: vscode.ExtensionContext) {
 	outputChannel = vscode.window.createOutputChannel("su-debug");
@@ -81,48 +43,22 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	// context.subscriptions.push(
-	// 	vscode.debug.onDidChangeBreakpoints((event: vscode.BreakpointsChangeEvent) => {
-	// 		procesChangeBreakpointsEvent(languageClient, event);
-	// 	})
+	// 	vscode.debug.onDidReceiveDebugSessionCustomEvent(
+	// 		(debugEvent: vscode.DebugSessionCustomEvent) => {
+	// 			console.log("onDidReceiveDebugSessionCustomEvent");
+	// 		}
+	// 	)
 	// );
 
-	vscode.debug.onDidReceiveDebugSessionCustomEvent(event => {
-		if (event.event === 'stopped') {
-			// ...
-		}
-	});
-
-	context.subscriptions.push(
-		vscode.debug.onDidReceiveDebugSessionCustomEvent(
-			(debugEvent: vscode.DebugSessionCustomEvent) => {
-				console.log("onDidReceiveDebugSessionCustomEvent");
-			}
-		)
-	);
-
-	context.subscriptions.push(
-		vscode.debug.onDidStartDebugSession(async (session: vscode.DebugSession) => {
-			const config = session.configuration;
-			//if (config.request !== "launch" || config.useTerminal || config.noDebug) { return; };
-			if (config.noDebug) {
-				return;
-			};
-
-			// session.getDebugProtocolBreakpoint().then((value: vscode.DebugProtocolBreakpoint) => {
-			// 	value.
-			// })
-			// const args: DebugProtocol.EvaluateArguments = {
-			// 	expression: ",eval $stdout.sync=true",
-			// 	context: "repl"
-			// };
-			// try {
-			// 	await session.customRequest("evaluate", args);
-			// } catch (err) {
-			// 	// We need to ignore the error because this request will be failed if the version of rdbg is older than 1.7. The `,command` API is introduced from version 1.7.
-			// 	pp(err);
-			// }
-		})
-	);
+	// context.subscriptions.push(
+	// 	vscode.debug.onDidStartDebugSession(async (session: vscode.DebugSession) => {
+	// 		const config = session.configuration;
+	// 		//if (config.request !== "launch" || config.useTerminal || config.noDebug) { return; };
+	// 		if (config.noDebug) {
+	// 			return;
+	// 		};
+	// 	})
+	// );
 
 	context.subscriptions.push(
 		vscode.debug.onDidTerminateDebugSession((session: vscode.DebugSession) => {
@@ -140,16 +76,16 @@ export function activate(context: vscode.ExtensionContext) {
 		)
 	);
 
-	context.subscriptions.push(
-		vscode.debug.onDidChangeBreakpoints(
-			(event: vscode.BreakpointsChangeEvent) => {
-				//vscode.debug.activeDebugSession?.customRequest("break");
+	// context.subscriptions.push(
+	// 	vscode.debug.onDidChangeBreakpoints(
+	// 		(event: vscode.BreakpointsChangeEvent) => {
+	// 			//vscode.debug.activeDebugSession?.customRequest("break");
 
-				console.log("onDidChangeBreakpoints");
+	// 			console.log("onDidChangeBreakpoints");
 
-			}
-		)
-	);
+	// 		}
+	// 	)
+	// );
 }
 
 export function deactivate() {
